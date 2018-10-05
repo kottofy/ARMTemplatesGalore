10deeb22cc422e412045d3a469525552bbc754d0
Kristin Ottofy <kottofy@outlook.com>
2018-10-05 11:05:30 -0400

    add az cli to setup

--- lib/setup.sh
+++ lib/setup.sh
@@ -2,7 +2,12 @@
 
 resourceGroupName={ExampleGroup}
 
-if [az group exists --name $resourceGroupName]; then 
+if ! command az >/dev/null; then
+  echo "Must install Azure CLI: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli" >&2
+  exit 1
+fi
+
+if az group exists --name $resourceGroupName; then 
     az group delete --name $resourceGroupName --yes
 fi
 az group create --name $resourceGroupName --location "East US"
