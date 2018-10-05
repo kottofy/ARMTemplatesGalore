217394749e1e6d058fafc71128a7a348ed15473c
Kristin Ottofy <kottofy@outlook.com>
2018-10-05 14:37:03 -0400

    update resource group name

--- KeyVault/scripts/deployKeyVault.sh
+++ KeyVault/scripts/deployKeyVault.sh
@@ -3,10 +3,10 @@
 vaultname={kristintest1234356}
 password={muhP4ss892734678}
 
-az group create --name examplegroup --location 'East US'
+az group create --name DevResourceGroupJB --location 'East US'
 az keyvault create \
   --name $vaultname \
-  --resource-group examplegroup \
+  --resource-group DevResourceGroupJB \
   --location 'East US' \
   --enabled-for-template-deployment true
 az keyvault secret set --vault-name $vaultname --name examplesecret --value $password
