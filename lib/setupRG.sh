6cb6d87dfe44b8d423d31648d2b683bf4e66d4c8
Kristin Ottofy <kottofy@outlook.com>
2018-10-16 13:26:53 -0400

    restoring cleanup of rg

--- lib/setupRG.sh
+++ lib/setupRG.sh
@@ -3,7 +3,7 @@
 resource_group_name="DevJBResourceGroup"
 location="eastus"
 
-# if az group exists --name "$resource_group_name"; then 
-#     az group delete --name "$resource_group_name" --yes
-# fi
+if az group exists --name "$resource_group_name"; then 
+    az group delete --name "$resource_group_name" --yes
+fi
 az group create --name "$resource_group_name" --location "$location"
